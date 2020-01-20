464af222a200d56d25f20101c22d1db1ee24714c
jxdxs <38853893+jxdxs@users.noreply.github.com>
2020-01-21 01:07:59 +0800

    Update l2tp.sh

--- l2tp/l2tp.sh
+++ l2tp/l2tp.sh
@@ -1,1 +1,1 @@
-wget https://raw.githubusercontent.com/jxdxs/docker/master/l2tp/l2tp.env && mv l2tp.env /etc/l2tp.env &&
+wget https://raw.githubusercontent.com/jxdxs/docker/master/l2tp/l2tp.env && mv l2tp.env /etc/l2tp.env && docker run -d --privileged -p 500:500/udp -p 4500:4500/udp --name l2tp --restart=always --env-file /etc/l2tp.env -v /lib/modules:/lib/modules teddysun/l2tp
