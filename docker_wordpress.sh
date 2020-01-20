a56a7c1c1f8411641797b3b83185a347070112bf
jxdxs <38853893+jxdxs@users.noreply.github.com>
2020-01-20 20:21:59 +0800

    Create docker_wordpress.sh

---
+++ docker_wordpress.sh
@@ -0,0 +1,1 @@
+sudo apt update && sudo apt-get update && apt-get install curl git -y && sudo curl -sSL https://get.docker.com/ | sh && apt install docker-compose -y && git clone https://github.com/jim51/docker-quick-wordpress.git && cd docker-quick-wordpress && docker-compose up -d
