9bf55a080f0facff5a24d823725b90e3d71165a2
jxdxs <38853893+jxdxs@users.noreply.github.com>
2020-01-21 02:00:41 +0800

    Delete wall.sh

--- wall.sh
+++
@@ -1,1 +0,0 @@
-wget https://raw.githubusercontent.com/jxdxs/docker/master/in_docker && wget https://raw.githubusercontent.com/jxdxs/docker/master/l2tp/l2tp.sh && wget https://raw.githubusercontent.com/jxdxs/docker/master/shadowsocks-go/ssgo.sh && sh in_docker.sh && sh l2tp.sh && sh ssgo.sh
