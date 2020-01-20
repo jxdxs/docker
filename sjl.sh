57b539721db8259f819068bbd50a823521a2f1e4
jxdxs <38853893+jxdxs@users.noreply.github.com>
2020-01-21 01:58:08 +0800

    Update sjl.sh

--- sjl.sh
+++ sjl.sh
@@ -1,6 +1,6 @@
-wget https://raw.githubusercontent.com/jxdxs/docker/master/in_docker && \
+wget https://raw.githubusercontent.com/jxdxs/docker/master/indocker && \
 wget https://raw.githubusercontent.com/jxdxs/docker/master/l2tp/l2tp.sh && \
 wget https://raw.githubusercontent.com/jxdxs/docker/master/shadowsocks-go/ssgo.sh && \
-sh in_docker.sh && \
+sh indocker.sh && \
 sh l2tp.sh && \
 sh ssgo.sh
