fad32db9fd33907e146c3ffd0949b8e9f391729a
jxdxs <38853893+jxdxs@users.noreply.github.com>
2020-01-21 02:18:38 +0800

    Delete ssgo.sh

--- shadowsocks-go/ssgo.sh
+++
@@ -1,1 +0,0 @@
-wget https://raw.githubusercontent.com/jxdxs/docker/master/shadowsocks-go/config.json && mkdir /etc/shadowsocks-go && mv config.json /etc/shadowsocks-go/config.json && docker run -d -p 10000:10000 -p 10000:10000/udp --name ss-go --restart=always -v /etc/shadowsocks-go:/etc/shadowsocks-go teddysun/shadowsocks-go
