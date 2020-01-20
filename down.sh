ff81cb9358986ef734a3d05512c3ac54cd2bf023
jxdxs <38853893+jxdxs@users.noreply.github.com>
2020-01-21 01:59:28 +0800

    Update and rename sjl.sh to down.sh

--- down.sh
+++ down.sh
@@ -1,6 +1,3 @@
 wget https://raw.githubusercontent.com/jxdxs/docker/master/indocker && \
 wget https://raw.githubusercontent.com/jxdxs/docker/master/l2tp/l2tp.sh && \
-wget https://raw.githubusercontent.com/jxdxs/docker/master/shadowsocks-go/ssgo.sh && \
-sh indocker.sh && \
-sh l2tp.sh && \
-sh ssgo.sh
+wget https://raw.githubusercontent.com/jxdxs/docker/master/shadowsocks-go/ssgo.sh
