efde5d2f127284114b00cc165297bd4785298eb1
jxdxs <38853893+jxdxs@users.noreply.github.com>
2020-01-21 02:22:26 +0800

    Update l2tp.sh

--- l2tp/l2tp.sh
+++ l2tp/l2tp.sh
@@ -1,6 +1,7 @@
 wget https://raw.githubusercontent.com/jxdxs/docker/master/l2tp/l2tp.env && \
 mv l2tp.env /etc/l2tp.env && \
-docker run -d --privileged -p 500:500/udp \
+docker run -d --privileged \
+-p 500:500/udp \
 -p 4500:4500/udp \
 --dns=8.8.8.8 \
 --dns=8.8.4.4 \
