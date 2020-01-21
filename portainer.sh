97d615e2cc125325213a93c275ad08c0df4f48a6
jxdxs <38853893+jxdxs@users.noreply.github.com>
2020-01-21 22:27:28 +0800

    Create portainer.sh

---
+++ portainer.sh
@@ -0,0 +1,6 @@
+docker pull portainer/portainer && \
+docker run -d -p 9000:9000 \
+    --restart=always \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    --name prtainer-test \
+    portainer/portainer
