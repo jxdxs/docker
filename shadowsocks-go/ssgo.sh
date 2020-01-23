234584b5ca9b72ba59cd516b5025313a889c3c77
jxdxs <38853893+jxdxs@users.noreply.github.com>
2020-01-23 11:38:56 +0800

    Update ssgo.sh

--- shadowsocks-go/ssgo.sh
+++ shadowsocks-go/ssgo.sh
@@ -1,6 +1,7 @@
 wget https://raw.githubusercontent.com/jxdxs/docker/master/shadowsocks-go/config.json && \
 mkdir /etc/shadowsocks-go && \
 mv config.json /etc/shadowsocks-go/config.json && \
+docker pull teddysun/shadowsocks-go && \
 docker run -d -p 10000:10000 \
 -p 10000:10000/udp \
 --dns=8.8.8.8 \
