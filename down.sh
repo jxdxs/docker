834811c7fee88434488d1486fd828ab2f5cc448a
jxdxs <38853893+jxdxs@users.noreply.github.com>
2020-01-23 11:34:06 +0800

    Update down.sh

--- down.sh
+++ down.sh
@@ -1,7 +1,9 @@
 wget https://raw.githubusercontent.com/jxdxs/docker/master/indocker.sh && \
 wget https://raw.githubusercontent.com/jxdxs/docker/master/l2tp/l2tp.sh && \
 wget https://raw.githubusercontent.com/jxdxs/docker/master/shadowsocks-go/ssgo.sh && \
+wget https://raw.githubusercontent.com/jxdxs/docker/master/portainer.sh && \
 ls && \
 sh indocker.sh && \
+sh portainer.sh && \
 sh l2tp.sh && \
 sh ssgo.sh
