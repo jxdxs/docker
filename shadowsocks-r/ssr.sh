e526569ed5e882ba3a3b05e338b65fc4aa7d4fa1
jxdxs <38853893+jxdxs@users.noreply.github.com>
2020-01-23 14:39:24 +0800

    Update ssr.sh

--- shadowsocks-r/ssr.sh
+++ shadowsocks-r/ssr.sh
@@ -1,6 +1,7 @@
 wget https://raw.githubusercontent.com/jxdxs/docker/master/shadowsocks-r/config.json && \
-mkdir /etc/shadowsocks-r/ && \
-mv config.json /etc/shadowsocks-r/ && \
+mkdir /etc/shadowsocks-r && \
+ls && \
+mv config.json /etc/shadowsocks-r/config.json && \
 docker pull teddysun/shadowsocks-r && \
 docker run -d \
 -p 20000:20000 \
