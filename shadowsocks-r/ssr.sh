822f563107b99eb66553b8a035f241342c042261
jxdxs <38853893+jxdxs@users.noreply.github.com>
2020-01-23 11:43:34 +0800

    Update ssr.sh

--- shadowsocks-r/ssr.sh
+++ shadowsocks-r/ssr.sh
@@ -1,1 +1,10 @@
-docker pull teddysun/shadowsocks-r
+wget https://raw.githubusercontent.com/jxdxs/docker/master/shadowsocks-r/config.json && \
+mv config.json /etc/shadowsocks-r/config.json && \
+docker pull teddysun/shadowsocks-r && \
+docker run -d \
+-p 9000:9000 \
+-p 9000:9000/udp \
+--name ssr \
+--restart=always \
+-v /etc/shadowsocks-r:/etc/shadowsocks-r \
+teddysun/shadowsocks-r
