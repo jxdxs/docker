98157d154442672a1210438a4c979c106ea84224
jxdxs <38853893+jxdxs@users.noreply.github.com>
2020-02-15 13:57:37 +0800

    Update iDl2tp.sh

--- iDl2tp.sh
+++ iDl2tp.sh
@@ -1,9 +1,10 @@
 wget https://raw.githubusercontent.com/jxdxs/docker/master/indocker.sh && \
 wget https://raw.githubusercontent.com/jxdxs/docker/master/l2tp/l2tp.sh && \
 wget https://raw.githubusercontent.com/jxdxs/docker/master/portainer.sh && \
-wget https://raw.githubusercontent.com/jxdxs/docker/master/shadowsocks-go/ssgo.sh && \
+wget https://raw.githubusercontent.com/jxdxs/docker/master/shadowsocks-go/ssgos.sh && \
 ls && \
 sh indocker.sh && \
 sh portainer.sh && \
 sh l2tp.sh && \
-sh ssgo.sh
+sh ssgos.sh && \
+rm indocker.sh portainer.sh l2tp.sh ssgos.sh
