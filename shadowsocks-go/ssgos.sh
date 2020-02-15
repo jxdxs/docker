f29d3619d8f22189c594620cddedd1a473970b74
jxdxs <38853893+jxdxs@users.noreply.github.com>
2020-02-15 13:02:19 +0800

    Create ssgos.sh

---
+++ shadowsocks-go/ssgos.sh
@@ -0,0 +1,14 @@
+wget https://raw.githubusercontent.com/jxdxs/docker/master/shadowsocks-go/config.json && \
+mkdir /etc/shadowsocks-go && \
+ls && \
+mv config.json /etc/shadowsocks-go/config.json && \
+docker pull teddysun/shadowsocks-go && \
+docker run -d \
+-p 10000-11000:10000-11000 \
+-p 10000-11000:10000-11000/udp \
+--dns=8.8.8.8 \
+--dns=8.8.4.4 \
+--name ss-go \
+--restart=always \
+-v /etc/shadowsocks-go:/etc/shadowsocks-go \
+teddysun/shadowsocks-go
