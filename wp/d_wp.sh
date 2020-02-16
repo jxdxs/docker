007135c65011666379d937f737bf956c3f485fe4
jxdxs <38853893+jxdxs@users.noreply.github.com>
2020-02-16 17:57:21 +0800

    Update d_wp.sh

--- wp/d_wp.sh
+++ wp/d_wp.sh
@@ -1,5 +1,4 @@
-mkdir wp
-wget https://raw.githubusercontent.com/jxdxs/docker/master/wp/uploads.ini
-wget https://raw.githubusercontent.com/jxdxs/docker/master/wp/wp.yml
-ls
+mkdir wp && cd wp
+curl -sSL https://raw.githubusercontent.com/jxdxs/docker/master/wp/uploads.ini > uploads.ini
+curl -sSL https://raw.githubusercontent.com/jxdxs/docker/master/wp/wp.yml > wp.yml
 docker-compose -f wp.yml up -d
