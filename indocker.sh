f417931e9bbbe2717b8e0eb53954df02b77994e1
jxdxs <38853893+jxdxs@users.noreply.github.com>
2020-02-18 17:11:21 +0800

    Update indocker.sh

--- indocker.sh
+++ indocker.sh
@@ -1,7 +1,8 @@
 sudo apt update && \
 sudo apt install curl -y && \
 sudo curl -sSL https://get.docker.com/ | sh && \
-systemctl enable docker
+sudo systemctl enable docker
+sudo apt remove docker-compose -y
 curl -L https://github.com/docker/compose/releases/download/1.25.4/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
