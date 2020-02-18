577e9c1ec47f1f5d805dc4c8591792dc9950c47c
jxdxs <38853893+jxdxs@users.noreply.github.com>
2020-02-18 12:34:36 +0800

    Create ssh.sh

---
+++ ubuntu18/ssh.sh
@@ -0,0 +1,3 @@
+sudo sed -i 's/^.*PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;
+sudo sed -i 's/^.*PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
+sudo service sshd restart
