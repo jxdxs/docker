a03b4f63c1249766e28bded251435fee79608b58
jxdxs <38853893+jxdxs@users.noreply.github.com>
2020-02-18 17:23:46 +0800

    Update docker-compose-ss.sh

--- shadowsocks-go/docker-compose-ss.sh
+++ shadowsocks-go/docker-compose-ss.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 mkdir ssgo
 wget -c https://raw.githubusercontent.com/jxdxs/docker/master/shadowsocks-go/configs.json -O ssgo/config.json
-wget -c https://raw.githubusercontent.com/jxdxs/docker/master/shadowsocks-go/ss.yml -O ssgo/ss.yml
+wget -c https://raw.githubusercontent.com/jxdxs/docker/master/shadowsocks-go/ss.yml -O ssgo/ssgo.yml
 docker-compose -f ssgo/ss.yml up -d
