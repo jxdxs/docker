60b42aa68d943abf71068f486d61b61841936068
jxdxs <38853893+jxdxs@users.noreply.github.com>
2020-02-20 21:47:11 +0800

    Update oneindex.sh

--- oneindex/oneindex.sh
+++ oneindex/oneindex.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 mkdir -p oneindex/config && mkdir oneindex/cache
 wget -P ./oneindex https://raw.githubusercontent.com/jxdxs/docker/master/oneindex/oneindex.yml
-docker-compose -f oneindex up -d
+docker-compose -f oneindex/oneindex.yml up -d
