d3841e3bcf9dad9f3569768fcb8daefb57a8c36e
jxdxs <38853893+jxdxs@users.noreply.github.com>
2020-02-21 23:26:28 +0800

    Create yjss_docker.sh

---
+++ ubuntu18/yjss_docker.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+curl -sSL https://raw.githubusercontent.com/jxdxs/docker/master/indocker.sh |bash
+curl -sSL https://raw.githubusercontent.com/jxdxs/docker/master/shadowsocks-go/docker-compose-ss.sh |bash
