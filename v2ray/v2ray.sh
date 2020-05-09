79dcd9cf06a58bef56d0dc93181bc480a4bbc035
jxdxs <38853893+jxdxs@users.noreply.github.com>
2020-05-09 09:24:21 +0800

    Update v2ray.sh

--- v2ray/v2ray.sh
+++ v2ray/v2ray.sh
@@ -1,1 +1,5 @@
 mkdir -p /etc/v2ray
+
+curl -o /etc/v2ray/config.json https://raw.githubusercontent.com/jxdxs/docker/master/v2ray/config.json
+
+docker run -d -p 20000:20000 --name v2ray --restart=always -v /etc/v2ray:/etc/v2ray teddysun/v2ray
