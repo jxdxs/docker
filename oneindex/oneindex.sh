d7059f1080f7b2bd883c83a107b5e83bb38977a7
jxdxs <38853893+jxdxs@users.noreply.github.com>
2020-10-17 12:14:04 +0800

    Update oneindex.sh

--- oneindex/oneindex.sh
+++ oneindex/oneindex.sh
@@ -1,5 +1,5 @@
 docker run -d --name oneindex \
-    -p 9080:80 --restart=always \
+    -p 88:80 --restart=always \
     -v ~/od/config:/var/www/html/config \
     -v ~/od/cache:/var/www/html/cache \
     -e REFRESH_TOKEN='0 * * * *' \
