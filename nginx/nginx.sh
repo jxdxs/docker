7b29deb9cbc020666268a5e2c62700c508ac0775
jxdxs <38853893+jxdxs@users.noreply.github.com>
2020-10-25 21:34:37 +0800

    Create nginx.sh

---
+++ nginx/nginx.sh
@@ -0,0 +1,5 @@
+wget -O /etc/nginx/conf.d/file.conf https://raw.githubusercontent.com/jxdxs/docker/master/nginx/file.conf
+
+wget -O /etc/nginx/conf.d/forw.conf https://raw.githubusercontent.com/jxdxs/docker/master/nginx/forw.conf
+
+rm /etc/
