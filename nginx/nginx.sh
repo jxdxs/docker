26ecb9bfc3697f9e6b8a322a8990fb81b08b2339
jxdxs <38853893+jxdxs@users.noreply.github.com>
2020-10-25 21:36:34 +0800

    Update nginx.sh

--- nginx/nginx.sh
+++ nginx/nginx.sh
@@ -2,4 +2,7 @@ wget -O /etc/nginx/conf.d/file.conf https://raw.githubusercontent.com/jxdxs/dock
 
 wget -O /etc/nginx/conf.d/forw.conf https://raw.githubusercontent.com/jxdxs/docker/master/nginx/forw.conf
 
-rm /etc/
+rm /etc/nginx/sites-enabled/default
+
+/etc/init.d/nginx reload
+
