5b825493f335b22b7d3e00c6c68a23eda3fe1fe8
jxdxs <38853893+jxdxs@users.noreply.github.com>
2020-10-26 02:34:00 +0000

    Update fb.sh

--- fb/fb.sh
+++ fb/fb.sh
@@ -1,10 +1,10 @@
-mkdir /var/www/html/fb
-touch /var/www/html/fb/filebrowser.db
-curl -o /var/www/html/fb/.filebrowser.json https://raw.githubusercontent.com/jxdxs/docker/master/fb/.filebrowser.json
+mkdir /root/fb
+touch /root/fb/filebrowser.db
+curl -o /root/fb/.filebrowser.json https://raw.githubusercontent.com/jxdxs/docker/master/fb/.filebrowser.json
 
-docker run -d \
-    -v /var/www/html/fb/srv:/srv \
-    -v /var/www/html/fb/filebrowser.db:/database.db \
-    -v /var/www/html/fb/.filebrowser.json:/.filebrowser.json \
+docker run -d \    
+    -v /root/fb/srv:/srv \
+    -v /root/fb/filebrowser.db:/database.db \
+    -v /root/fb/.filebrowser.json:/.filebrowser.json \
     -p 86:80 \
     xhfz/fb
