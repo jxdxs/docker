8ca9ab178c629b44901de1b20cb057d823a56d09
jxdxs <38853893+jxdxs@users.noreply.github.com>
2020-10-29 08:13:05 +0000

    Create one.sh

---
+++ bt/one.sh
@@ -0,0 +1,3 @@
+wget -O aria2dash https://github.com/Masterchiefm/Aria2Dash/raw/master/install.sh && bash aria2dash -a n -d /var/www/html -p naplnd1989
+apt install nginx -t
+rm /etc/nginx/sites-enabled/default
