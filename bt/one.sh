b2ea1d7477f48f7e5fd49794d4b1054c3eb0d73d
jxdxs <38853893+jxdxs@users.noreply.github.com>
2020-10-29 08:14:57 +0000

    Update one.sh

--- bt/one.sh
+++ bt/one.sh
@@ -1,3 +1,5 @@
 wget -O aria2dash https://github.com/Masterchiefm/Aria2Dash/raw/master/install.sh && bash aria2dash -a n -d /var/www/html -p naplnd1989
-apt install nginx -t
+apt install nginx -y
 rm /etc/nginx/sites-enabled/default
+curl -o /etc/nginx/conf.d/file.conf https://raw.githubusercontent.com/jxdxs/docker/master/nginx/file.conf
+/etc/init.d/nginx reload
