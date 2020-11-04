8d7f2f96925e8675df0a8e641c9824c4465ba03b
jxdxs <38853893+jxdxs@users.noreply.github.com>
2020-11-04 09:16:40 +0000

    Update dnsmasq.sh

--- dnsmasq.sh
+++ dnsmasq.sh
@@ -1,6 +1,5 @@
 apt install dnsmasq -y 
-mv /etc/dnsmasq.conf /etc/dnsmasq.conf.bak
-touch /etc/dnsmasq.conf
+cp /etc/dnsmasq.conf /etc/dnsmasq.conf.bak
 echo 'resolv-file=/etc/resolv.conf' >> /etc/dnsmasq.conf
 echo 'strict-order' >> /etc/dnsmasq.conf
 /etc/init.d/dnsmasq restart
