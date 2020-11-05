3a1627f6fbaeb9a46fc8dcd79a78df20bad06c02
jxdxs <38853893+jxdxs@users.noreply.github.com>
2020-11-05 01:36:29 +0000

    Update dnsmasq.sh

--- dnsmasq/dnsmasq.sh
+++ dnsmasq/dnsmasq.sh
@@ -6,6 +6,7 @@ docker run \
     -p 53:53/udp \
     -p 5380:8080 \
     -v /opt/dnsmasq.conf:/etc/dnsmasq.conf \
+    --restart=always \
     --log-opt "max-size=100m" \
     -e "HTTP_USER=foo" \
     -e "HTTP_PASS=bar" \
