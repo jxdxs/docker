49b5517e1853ed8d46339737b5091cad562330f7
jxdxs <38853893+jxdxs@users.noreply.github.com>
2020-11-05 01:30:56 +0000

    Delete dnsmasq.sh

--- dnsmasq.sh
+++
@@ -1,6 +0,0 @@
-apt install dnsmasq -y 
-cp /etc/dnsmasq.conf /etc/dnsmasq.conf.bak
-echo 'resolv-file=/etc/resolv.conf' >> /etc/dnsmasq.conf
-echo 'strict-order' >> /etc/dnsmasq.conf
-/etc/init.d/dnsmasq restart
-netstat -tunlp|grep 53
