b39634750cb563cd2805a41c0a1a625198a84683
jxdxs <38853893+jxdxs@users.noreply.github.com>
2021-11-18 21:06:33 +0100

    Update indocker.sh

--- indocker.sh
+++ indocker.sh
@@ -1,5 +1,9 @@
 apt install curl -y
-curl -sSL https://get.docker.com/ | sh 
+curl -sSL https://get.docker.com/ | sh
 systemctl enable docker
-apt install -y docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.29.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+ls /usr/local/bin/
+chmod +x /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+docker-compose -v
 echo "docker install ok"
