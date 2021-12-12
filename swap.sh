71686f24d565ff8013e25afc481b22c589fd012c
jxdxs <38853893+jxdxs@users.noreply.github.com>
2021-12-12 12:40:25 +0100

    Update swap.sh

--- swap.sh
+++ swap.sh
@@ -1,5 +1,5 @@
 mkdir swap && \
-sudo dd if=/dev/zero of=./swap/swapfile bs=1M count=1024 && \
-sudo mkswap ./swap/swapfile && \
-sudo swapon ./swap/swapfile && \
-echo /root/swap/swapfile swap swap defaults 0 0 >> /etc/fstab
+sudo dd if=/dev/zero of=/opt/swap/swapfile bs=1M count=1024 && \
+sudo mkswap /opt/swap/swapfile && \
+sudo swapon /opt/swap/swapfile && \
+echo /opt/swap/swapfile swap swap defaults 0 0 >> /etc/fstab
