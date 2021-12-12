af9da03f531ee74acb3512dcd3dc243d78ebcbc9
jxdxs <38853893+jxdxs@users.noreply.github.com>
2021-12-12 11:53:38 +0100

    Update discuz.sh

--- discuz.sh
+++ discuz.sh
@@ -1,2 +1,2 @@
 docker run --name  mysql -e MYSQL_ROOT_PASSWORD=root -d mysql:5.7.22
-docker run --name  discuz --privileged=true -v /data:/var/www/html --link mysql:mysql -p 80:80 -d skyzhou/docker-discuz
+docker run --name  discuz --privileged=true -v /root/discuz:/var/www/html --link mysql:mysql -p 99:80 -d skyzhou/docker-discuz
