56e1b8999687f7c0d0d55148533a9d1fc625b050
jxdxs <38853893+jxdxs@users.noreply.github.com>
2022-11-06 17:00:10 +0800

    Update yjss.sh

--- yjss.sh
+++ yjss.sh
@@ -1,2 +1,3 @@
+curl https://raw.githubusercontent.com/jxdxs/docker/master/aptup.sh | bash
 curl https://raw.githubusercontent.com/jxdxs/docker/master/indocker.sh | bash
 curl https://raw.githubusercontent.com/jxdxs/docker/master/shadowsocks-go/docker-compose-ss.sh | bash
