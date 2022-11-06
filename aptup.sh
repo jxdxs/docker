8be8dd78e87d0ad13ee0a6d93f50cf746d383276
jxdxs <38853893+jxdxs@users.noreply.github.com>
2022-11-06 17:07:55 +0800

    Update aptup.sh

--- aptup.sh
+++ aptup.sh
@@ -1,2 +1,17 @@
+cat > /etc/apt/sources.list << EOF
+deb http://deb.debian.org/debian/ bullseye main contrib non-free
+deb-src http://deb.debian.org/debian/ bullseye main contrib non-free
+
+deb http://deb.debian.org/debian/ bullseye-updates main contrib non-free
+deb-src http://deb.debian.org/debian/ bullseye-updates main contrib non-free
+
+deb http://deb.debian.org/debian/ bullseye-backports main contrib non-free
+deb-src http://deb.debian.org/debian/ bullseye-backports main contrib non-free
+
+deb http://deb.debian.org/debian-security/ bullseye-security main contrib non-free
+deb-src http://deb.debian.org/debian-security/ bullseye-security main contrib non-free
+EOF
+
+
 apt update
 apt install -y curl wget vnstat vim htop vnstat screen
