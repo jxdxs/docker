4efdd0137e27c75522c9ff028fecf6830f42cd5c
jxdxs <38853893+jxdxs@users.noreply.github.com>
2022-11-08 12:46:48 +0800

    Delete onetoss.sh

--- onetoss.sh
+++
@@ -1,5 +0,0 @@
-apt-get update && apt-get install -y sudo vnstat vim curl wget iftop htop screen axel
-curl https://raw.githubusercontent.com/jxdxs/docker/master/bbr.sh | bash
-curl https://raw.githubusercontent.com/jxdxs/docker/master/swap.sh | bash
-curl https://raw.githubusercontent.com/jxdxs/docker/master/indocker.sh | bash
-curl https://raw.githubusercontent.com/jxdxs/docker/master/shadowsocks-go/docker-compose-ss.sh | bash
