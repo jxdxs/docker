74f54b54383ed3a5bd62fcecb87d8778fac3eace
jxdxs <38853893+jxdxs@users.noreply.github.com>
2022-11-11 20:28:44 +0800

    Update fb.sh

--- fb/fb.sh
+++ fb/fb.sh
@@ -1,10 +1,7 @@
-mkdir /root/fb
-touch /root/fb/filebrowser.db
-curl -o /root/fb/.filebrowser.json https://raw.githubusercontent.com/jxdxs/docker/master/fb/.filebrowser.json
-
 docker run -d \
-    -v /home/yun/fb:/srv \
-    -v /root/fb/filebrowser.db:/database.db \
-    -v /root/fb/.filebrowser.json:/.filebrowser.json \
-    -p 86:80 \
-    xhfz/fb
+--name=fb \
+-v /mnt/sdb/yun:/srv \
+-p 88:80 \
+--restart=always \
+filebrowser/filebrowser
+
